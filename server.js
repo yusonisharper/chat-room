9f535b491ee392f29d8b4a08a860504aa2d31dd8
yusonisharper <wang757566564@gmail.com>
2024-06-01 19:36:43 -0700

    Allow user to edit, delete or search a message

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const roomHandler = require('./controllers/room.js')(getRoomList);
 const app = express();
 const port = 8080;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { AsyncLocalStorage } = require('async_hooks');
 const uri = "mongodb+srv://wang757566564:XxlJI4Q0Mx969BQL@cluster0.y10dzvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -76,7 +76,7 @@ async function handleMessageRequest(req, res) {
         // Sort by created date in ascending order
         sort: { created_date: 1 },
         // Include only the fields we need
-        projection: { _id: 0, roomID: 1, name: 1, body: 1, created_date: 1 },
+        projection: { _id: 1, roomID: 1, name: 1, body: 1, created_date: 1 },
     };
     const cursor = collection.find(query, options);
     // Print each document
@@ -113,12 +113,6 @@ app.post('/:roomID-delete', async (req, res) => {
     const database = client.db('my_db');
     const collection = database.collection('rooms');
     const query = {'roomID': req.params.roomID}; // Adjust the query as needed
-    const options = {
-        // Sort by created date in ascending order
-        sort: { created_date: 1 },
-        // Include only the fields we need
-        projection: { _id: 0, roomID: 1, name: 1, body: 1, created_date: 1 },
-    };
     const result = await collection.deleteOne(query);
     if (result.deletedCount === 1) {
         res.json('room deleted');
@@ -126,6 +120,49 @@ app.post('/:roomID-delete', async (req, res) => {
         res.status(404).json('room not found');
     }
 })
+
+app.post('/:roomID/messages-delete', async (req, res) => {
+    const database = client.db('my_db');
+    const collection = database.collection('messages');
+    const query = {'_id': new ObjectId(req.body.msgID)}; // Adjust the query as needed
+    const result = await collection.deleteOne(query);
+    if (result.deletedCount === 1) {
+        res.json('message deleted');
+    } else {
+        res.status(404).json('msg not found');
+    }
+});
+
+// app.post('/:roomID/messages-search', async (req, res) => {
+//     const database = client.db('my_db');
+//     const collection = database.collection('messages');
+//     const query = { 
+//         roomID: req.params.roomID, 
+//         $text: { $search: req.body.search }
+//     }; // Ensure you have a text index on the collection
+//     const options = {
+//         // Sort by created date in ascending order
+//         sort: { created_date: 1 },
+//         // Include only the fields we need
+//         projection: { _id: 1, roomID: 1, name: 1, body: 1, created_date: 1 },
+//     };
+//     const messages = await collection.find(query).toArray();
+//     console.log(messages);
+//     res.json(messages);
+// });
+
+app.post('/:roomID/messages-update', async (req, res) => {
+    const database = client.db('my_db');
+    const collection = database.collection('messages');
+    const query = {_id: new ObjectId(req.body.msgID)}; // Adjust the query as needed
+    const update = {$set:{body: req.body.text}};
+    const result = await collection.updateOne(query, update);
+    if (result.modifiedCount === 1) {
+        res.json('message updated');
+    } else {
+        res.status(404).json('msg not found');
+    }
+});
 
 app.post('/:roomID/messages', async (req, res) => {
     const { message, nickName, date } = req.body;
